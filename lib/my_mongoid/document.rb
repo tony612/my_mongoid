ee5036906df8cd1460c050876e83d80b61bab6ac
Tony Han <h.bing612@gmail.com>
2014-02-25 22:00:38 +0800

    add attributes, the reader and writer methods of it

--- lib/my_mongoid/document.rb
+++ lib/my_mongoid/document.rb
@@ -2,6 +2,8 @@ module MyMongoid
   module Document
     extend ActiveSupport::Concern
 
+    attr_accessor :attributes
+
     module ClassMethods
     end
 
@@ -11,6 +13,28 @@ module MyMongoid
       end
 
       MyMongoid.register_model(self)
+    end
+
+    def initialize(attrs)
+      raise ArgumentError unless attrs.is_a?(Hash)
+      self.attributes = attrs
+    end
+
+    def attributes
+      @attributes ||= {}
+    end
+
+    def read_attribute(key)
+      key = key.to_s
+      attributes[key]
+    end
+
+    def write_attribute(key, value)
+      attributes[key] = value
+    end
+
+    def new_record?
+      true
     end
   end
 end
