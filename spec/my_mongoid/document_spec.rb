f663dae41d0432f65f09bf868c4123e7d6093cdf
Tony Han <h.bing612@gmail.com>
2014-02-28 00:38:30 +0800

    add test for Document

---
+++ spec/my_mongoid/document_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+class Event
+  include MyMongoid::Document
+end
+
+describe MyMongoid::Document do
+  it 'is a module' do
+    expect(MyMongoid::Document).to be_a(Module)
+  end
+
+  describe '.is_mongoid_model?' do
+    it 'returns true' do
+      expect(Event.is_mongoid_model?).to be(true)
+    end
+  end
+
+  describe '#new_record?' do
+    it 'returns true' do
+      expect(Event.new.new_record?).to be(true)
+    end
+  end
+
+  describe '#initialize' do
+    it 'accepts a hash' do
+      expect(Event.new({})).to be_a(Event)
+    end
+
+    it 'raises ArgumentError when passed a non-hash' do
+      expect {
+        Event.new("foo")
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'process_attributes' do
+      event = Event.new
+      expect(event).to receive(:process_attributes)
+      event.send(:initialize)
+    end
+
+    it 'accepts a block to operate the object' do
+      event_block = nil
+      event = Event.new do |e|
+        event_block = e
+      end
+      expect(event).to eql(event_block)
+    end
+
+  end
+end
